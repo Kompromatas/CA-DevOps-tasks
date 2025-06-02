d357c22f001519e3561d52fd7ff8db0d40e61f1c
Darius <Darius.pukstys@outlook.com>
2025-06-02 21:24:31 +0300

    16 pamoka

--- 16/8-task.sh
+++ 16/8-task.sh
@@ -6,11 +6,12 @@ replace_word() {
     local WORD2=$3
 
     # Replace word using yq
-    yq eval 'walk(if type == "string" then gsub("'"$WORD1"'"; "'"$WORD2"'") else . end)' "$FILE" > temp.yaml && mv temp.yaml "$FILE"
+    #yq eval 'walk(if type == "string" then gsub("'"$WORD1"'"; "'"$WORD2"'") else . end)' "$FILE" > temp.yaml && mv temp.yaml "$FILE"
+    yq eval '(.. | select(tag == "!!str") |= sub("'"$WORD1"'"; "'"$WORD2"'"))' "$FILE" > temp.yaml && mv temp.yaml "$FILE"
 
     # Print the updated YAML
     echo "Word '$WORD1' replaced with '$WORD2' in $FILE !!!!!"
-    
+
     cat "$FILE"
 
     
