72fd04325e32895e7ce75c69e071a847a21d31fd
Darius <Darius.pukstys@outlook.com>
2025-06-02 21:05:20 +0300

    16 pamoka

--- 16/7-task.sh
+++ 16/7-task.sh
@@ -3,12 +3,12 @@
 # Variables
 FILE="3-task.json"  # Your JSON file
 WORD1="cooking"
-WORD="cooding"
+WORD2="cooding"
 
 # Replace word using jq
 jq --arg target "$WORD1" --arg replace "$WORD2" '
     walk(if type == "string" then gsub($target; $replace) else . end)
 ' "$FILE" > temp.json && mv temp.json "$FILE"
 
-echo "✅ '$WORD1' replaced with '$WORD2' in $FILE"
+echo "Word '$WORD1' replaced with '$WORD2' in $FILE !!!!!"
 jq . "$FILE"  # Display the updated JSON file
