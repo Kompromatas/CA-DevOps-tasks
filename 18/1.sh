fbf8b6f9c45a4e9994c731a63e3ef646995ebf37
Darius <Darius.pukstys@outlook.com>
2025-06-05 21:24:40 +0300

    18 pamoka

---
+++ 18/1.sh
@@ -0,0 +1,24 @@
+#! /bin/bash    
+GROUP='ca-devops-1'
+LOCATION='eastus'
+IMAGE='Canonical:ubuntu-24_04-lts:server:latest'
+SIZE='Standard_B1s'
+
+az storage account create --name storageacc181 --resource-group $GROUP --location $LOCATION --sku Standard_LRS
+
+az storage container create -- name blob18 --account-name storageacc181 --public-access blob
+
+az network nsg create --resource-group $GROUP --name nsg18
+
+az network nsg rule create --resource-group $GROUP --nsg-name nsg18 --name allow-SSH --protocol Tcp --priority 102 --destination-port-range 22 --access Allow --direction Inbound --source-address-prefix '*' --source-port-range '*' --destination-address-prefix '*'
+
+az vm create \
+  --resource-group $GROUP \
+  --name 'vm-devops-18-2' \
+  --image $IMAGE \
+  --admin-username 'adm_darius' \
+  --generate-ssh-keys \
+  --location $LOCATION \
+  --size $SIZE \
+  --nsg nsg18  
+
