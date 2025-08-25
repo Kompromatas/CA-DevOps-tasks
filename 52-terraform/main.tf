794b0ea184683b1d1e35a16fdc70e9eee28a76fe
Darius <Darius.pukstys@outlook.com>
2025-08-25 21:37:41 +0300

    52 pamoka

--- 52-terraform/main.tf
+++ 52-terraform/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "server1" {
   }
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World 1" > index.html
+              echo "Hello, World from terraform build" > index.html
               python3 -m http.server 8080 &
               EOF
 
