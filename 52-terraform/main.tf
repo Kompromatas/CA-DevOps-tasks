b2b60141362605c6eccc49ad32c0635bca916395
Darius <Darius.pukstys@outlook.com>
2025-08-25 22:14:31 +0300

    !!!!!

--- 52-terraform/main.tf
+++ 52-terraform/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "server1" {
   }
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World from terraform build!!!!" > index.html
+              echo "Hello, World from terraform build!!!!!" > index.html
               python3 -m http.server 8080 &
               EOF
 
