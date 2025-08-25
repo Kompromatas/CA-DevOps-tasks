fe3ed498fe529d0290a296f6d7f0d2204e644e31
Darius <Darius.pukstys@outlook.com>
2025-08-25 22:03:01 +0300

    52 pamoka

--- 52-terraform/variables.tf
+++ 52-terraform/variables.tf
@@ -1,9 +1,3 @@
-variable "profile" {
-  description = "The AWS profile for authentication"
-  type        = string
-  default     = "default"
-}
-
 variable "region" {
   description = "The AWS region to deploy resources to"
   type        = string
